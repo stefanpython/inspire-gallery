bc7750edaaad25ae44705ac6124fbb35d955e5cf
stefanpython <stefanhtml@yahoo.com>
2025-02-25 12:37:36 +0200

    Implement SearchContext.tsx

--- app/components/Gallery/SearchBar.tsx
+++ app/components/Gallery/SearchBar.tsx
@@ -17,7 +17,7 @@ const SearchBar = () => {
   };
 
   return (
-    <nav className="w-full bg-white shadow-md px-6 py-4">
+    <nav className="w-full bg-white shadow-md px-6 py-2 border-b border-gray-200">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <h1 className={`${orbitron.className} text-3xl font-bold`}>
           <Image
