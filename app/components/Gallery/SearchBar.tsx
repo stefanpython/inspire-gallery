aba65b3a9e4659c8b80755cc78bc16533a8f03f7
stefanpython <stefanhtml@yahoo.com>
2025-02-26 12:57:47 +0200

    Remove padding

--- app/components/Gallery/SearchBar.tsx
+++ app/components/Gallery/SearchBar.tsx
@@ -33,7 +33,7 @@ const SearchBar = () => {
   };
 
   return (
-    <nav className="w-full bg-white shadow-md px-6 py-2 border-b border-gray-200">
+    <nav className="w-full bg-white shadow-md px-6 border-b border-gray-200">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <button
           onClick={handleReset}
